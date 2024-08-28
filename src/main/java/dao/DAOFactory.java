ae7ce434d2395091afddb49749ece4c8112a441a
rashmikaz <phrashmika@gmail.com>
2024-08-28 17:58:39 +0530

    crud dao,dao factory

--- src/main/java/dao/DAOFactory.java
+++ src/main/java/dao/DAOFactory.java
@@ -1,4 +1,32 @@
 package dao;
 
 public class DAOFactory {
+    private static DAOFactory daoFactory;
+
+    private DAOFactory(){
+
+    }
+
+    public static DAOFactory getDaoFactory(){
+        return daoFactory == null ? daoFactory = new DAOFactory() : daoFactory;
+    }
+
+    public enum DAOTypes{
+        CUSTOMER,ITEM,ORDER,ORDER_DETAIL
+    }
+
+    public SuperDAO getDao(DAOTypes daoTypes){
+        switch (daoTypes){
+            case CUSTOMER:
+                return new CustomerDAOImpl();
+            case ITEM:
+                return new ItemDAOImpl();
+            case ORDER:
+                return new OrderDAOImpl();
+            case ORDER_DETAIL:
+                return new OrderDetailDAOImpl();
+            default:
+                return null;
+        }
+    }
 }
